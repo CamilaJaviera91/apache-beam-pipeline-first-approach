eea606f8c7b78c6a58c6286f653ff429a5b30fbe
camilajaviera <cjmunozn@hotmail.com>
2025-02-27 16:54:18 -0300

    feat(TF): edit folder to save csv file

--- pipelines/4. pipeline_filtered_T_F_csv.py
+++ pipelines/4. pipeline_filtered_T_F_csv.py
@@ -1,15 +1,7 @@
 import apache_beam as beam  # Apache Beam for data processing pipeline
 from sklearn.datasets import load_linnerud as dataset  # Load the Linnerud dataset from scikit-learn
 import pandas as pd  # Pandas for handling data in DataFrame format
-import os  # OS module for file and directory operations
 import glob  # Glob module to search for files with a specific pattern
-
-# Get the current working directory
-folder = os.getcwd()
-
-# Create a new subfolder for storing the output
-download_folder = os.path.join(folder, "download")
-os.makedirs(download_folder, exist_ok=True)  # Ensure the folder exists
 
 # Load the Linnerud dataset
 dt = dataset()
@@ -29,7 +21,7 @@ def add_new_field(row):
     row['Chins(>10)'] = row['Chins'] > 10  # Create a new field based on condition
     return row
 
-def run_pipeline(output_csv_path):
+def run_pipeline(output_path):
     """Runs an Apache Beam pipeline to filter 'Chins' > 10 and save output to CSV."""
     
     with beam.Pipeline() as pipeline:
@@ -46,27 +38,13 @@ def run_pipeline(output_csv_path):
         result_pcoll | 'Print Results' >> beam.Map(print)
 
         # Write output to a single CSV file
-        result_pcoll | 'Write to CSV' >> beam.io.WriteToText(
-        output_csv_path,
-        file_name_suffix='.csv',
-        num_shards=1,
-        header="Chins,Situps,Jumps,Chins(>10)"
-    )
+        result_pcoll | 'Write Results' >> beam.io.WriteToText(output_path, shard_name_template='')
 
-    print(f"Pipeline completed. Data saved to {output_csv_path}.csv")
+    print(f"Pipeline completed. Data saved to {output_path}.csv")
 
 if __name__ == "__main__":
 
     # Define output file path
-    output_csv = os.path.join(download_folder, "chins_filtered")
+    output_path = './download/chins_filtered_3.csv'
 
-    run_pipeline(output_csv)
-
-    # Rename the generated CSV file (optional step)
-    generated_files = glob.glob(os.path.join(download_folder, "chins_filtered-*.csv"))
-    if generated_files:
-        final_path = os.path.join(download_folder, "chins_filtered_3.csv")
-        os.rename(generated_files[0], final_path)
-        print(f"File saved to: {final_path}")
-    else:
-        print("No output file found!")
+    run_pipeline(output_path)
