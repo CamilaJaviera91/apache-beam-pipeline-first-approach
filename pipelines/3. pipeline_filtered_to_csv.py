0418ad922edce88bc615bb27aed6ddd28b7467b5
camilajaviera <cjmunozn@hotmail.com>
2025-02-27 17:03:23 -0300

    feat(TF): edit folder to save csv file

--- pipelines/3. pipeline_filtered_to_csv.py
+++ pipelines/3. pipeline_filtered_to_csv.py
@@ -2,15 +2,6 @@
 import apache_beam as beam  # Apache Beam for data processing pipeline
 from sklearn.datasets import load_linnerud as dataset  # Load the Linnerud dataset from scikit-learn
 import pandas as pd  # Pandas for handling data in DataFrame format
-import os  # OS module for file and directory operations
-import glob  # Glob module to search for files with a specific pattern
-
-# Get the current working directory
-folder = os.getcwd()
-
-# Create a new subfolder for storing the output
-download_folder = os.path.join(folder, "download")
-os.makedirs(download_folder, exist_ok=True)  # Ensure the folder exists
 
 # Load the Linnerud dataset
 dt = dataset()
@@ -25,7 +16,7 @@ def extract_values(row):
     """Extracts values from the dictionary and returns them as a comma-separated string."""
     return ', '.join(map(str, row.values()))
 
-def run_pipeline(output_csv_path):
+def run_pipeline(output_path):
     """Runs an Apache Beam pipeline to filter 'Chins' > 10 and save output to CSV."""
     
     with beam.Pipeline() as pipeline:
@@ -39,30 +30,17 @@ def run_pipeline(output_csv_path):
         result_pcoll = filtered_data | 'Extract Values' >> beam.Map(extract_values)
 
         # Write output to CSV format
-        result_pcoll | 'Write to CSV' >> beam.io.WriteToText(
-        output_csv_path,
-        file_name_suffix='.csv',
-        num_shards=1,
-        header="Chins,Situps,Jumps"
-    )
+        result_pcoll | 'Write Results' >> beam.io.WriteToText(output_path, shard_name_template='')
 
         # Print results for debugging purposes
         result_pcoll | 'Print Results' >> beam.Map(print)
 
-    print(f"Pipeline completed. Data saved to {output_csv_path}.csv")
+    print(f"Pipeline completed. Data saved to {output_path}.csv")
 
 if __name__ == '__main__':
+
     # Define output file path
-    output_csv = os.path.join(download_folder, "chins_filtered")
+    output_path = './download/chins_filtered_2.csv'
 
     # Run Apache Beam pipeline
-    run_pipeline(output_csv)
-
-    # Rename the generated CSV file (optional step)
-    generated_files = glob.glob(os.path.join(download_folder, "chins_filtered-*.csv"))
-    if generated_files:
-        final_path = os.path.join(download_folder, "chins_filtered_2.csv")
-        os.rename(generated_files[0], final_path)
-        print(f"File saved to: {final_path}")
-    else:
-        print("No output file found!")
+    run_pipeline(output_path)
